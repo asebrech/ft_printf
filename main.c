6b3ef1d0e5b56a7ed281c3e0fa2abe388b0a35be
alois <aloisebrechts@gmail.com>
2021-05-01 16:22:16 +0200

    cspdiuxX%

--- main.c
+++ main.c
@@ -6,15 +6,24 @@
 /*   By: asebrech <asebrech@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 00:06:35 by asebrech          #+#    #+#             */
-/*   Updated: 2021/05/01 12:16:51 by asebrech         ###   ########.fr       */
+/*   Updated: 2021/05/01 15:53:30 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <stdio.h>
 
+char		c = 'Z';
+char		*s = "Hello";
+int		d = -2147483648;
+int		i = 2147483647;
+unsigned int	u = 4294967295;
+unsigned int	x = 4294967295;
+unsigned int	X = 4294967295;
+char		*p = "test";
+
 int	main(void)
 {
-	ft_printf("this %X %x is %% %s the %c way\n", 117, 7567, "hello", 'X');
-	printf("this %X %x is %% %s the %c way\n", 117, 7567, "hello", 'X');
+	ft_printf("ft_printf :: c : %c / s : %s / d: %d i: %i / u : %u / x : %x / X : %X / %% : %% / p : %p\n", c, s, d, i, u, x, X, p);
+	printf("OR printf :: c : %c / s : %s / d: %d i: %i / u : %u / x : %x / X : %X / %% : %% / p : %p\n", c, s, d, i, u, x, X, p);
 }
--- srcs/ft_printf_convert.c
+++ srcs/ft_printf_convert.c
@@ -6,14 +6,16 @@
 /*   By: asebrech <asebrech@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/01 12:23:08 by asebrech          #+#    #+#             */
-/*   Updated: 2021/05/01 12:34:29 by asebrech         ###   ########.fr       */
+/*   Updated: 2021/05/01 15:45:17 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-
+#include <stdio.h>
 void	ft_convert(const char *format, va_list args)
 {
+	void	*ptr;
+
 	if (*format == '%')
 		ft_putchar_fd('%', 1);
 	else if (*format == 'd' || *format == 'i')
@@ -25,7 +27,18 @@ void	ft_convert(const char *format, va_list args)
 	else if (*format == 's')
 		ft_putstr_fd(va_arg(args, char *), 1);
 	else if (*format == 'x')
-		ft_putnbr_base_fd(va_arg(args, int), "0123456789abcdef", 1);
+		ft_putnbr_base_fd(va_arg(args, unsigned int), "0123456789abcdef", 1);
 	else if (*format == 'X')
 		ft_putnbr_base_fd(va_arg(args, unsigned int), "0123456789ABCDEF", 1);
+	else if (*format == 'p')
+	{
+		ptr = va_arg(args, void *);
+		if (ptr == NULL)
+			ft_putstr_fd("(nil)", 1);
+		else
+		{
+			ft_putstr_fd("0x", 1);
+			ft_putnbr_base_fd((long int)ptr, "0123456789abcdef",1);
+		}
+	}
 }
