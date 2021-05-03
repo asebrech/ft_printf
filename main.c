d456d8a33bd27b80872c83c166b5d332af4e7479
alois <aloisebrechts@gmail.com>
2021-05-03 11:05:46 +0200

    main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: asebrech <asebrech@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 00:06:35 by asebrech          #+#    #+#             */
-/*   Updated: 2021/05/01 15:53:30 by asebrech         ###   ########.fr       */
+/*   Updated: 2021/05/02 10:19:33 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 #include <stdio.h>
 
 char		c = 'Z';
-char		*s = "Hello";
-int		d = -2147483648;
+char		*s = "This is the way";
+int		d = 10;
 int		i = 2147483647;
 unsigned int	u = 4294967295;
 unsigned int	x = 4294967295;
@@ -25,5 +25,7 @@ char		*p = "test";
 int	main(void)
 {
 	ft_printf("ft_printf :: c : %c / s : %s / d: %d i: %i / u : %u / x : %x / X : %X / %% : %% / p : %p\n", c, s, d, i, u, x, X, p);
-	printf("OR printf :: c : %c / s : %s / d: %d i: %i / u : %u / x : %x / X : %X / %% : %% / p : %p\n", c, s, d, i, u, x, X, p);
+	printf("OR printf :: c : %c / s : %s / d: %d i: %i / u : %u / x : %x / X : %X / %% : %% / p : %p\n\n", c, s, d, i, u, x, X, p);
+	printf("printf test\n\n");
+	printf("printf : %07.*d\n", d, i);
 }
