727866756bc151abd7febaae088ec083a2d32e69
Aloïs <aloisebrechts@gmail.com>
2021-05-12 10:06:07 +0200

    first final

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: asebrech <asebrech@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 00:06:35 by asebrech          #+#    #+#             */
-/*   Updated: 2021/05/11 18:48:00 by asebrech         ###   ########.fr       */
+/*   Updated: 2021/05/12 10:05:11 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int				test;
 int				test1;
 char			c = '%';
 char			*s = "This is the way";
-int				d = -7567;
+int				d = 117;
 int				i = 22;
 unsigned int	u = 7567;
 unsigned int	x = 4294967295;
@@ -30,8 +30,8 @@ int	main(void)
 	printf("OR printf :: c : %c / s : %s / d: %d i: %i / u : %u / x : %x / X : %X / %% : %% / p : %p\n\n", c, s, d, i, u, x, X, p);
 	
 
-	test = ft_printf("FT : %22s\n", s);
-	test1 = printf("OR : %22s\n", s);
+	test = ft_printf("FT : %5.1s\n", s);
+	test1 = printf("OR : %5.1s\n", s);
 
 	ft_printf("FT : %d\n", test);
 	ft_printf("OR : %d\n", test1);
--- srcs/ft_convert_str.c
+++ srcs/ft_convert_str.c
@@ -6,17 +6,47 @@
 /*   By: asebrech <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 18:27:18 by asebrech          #+#    #+#             */
-/*   Updated: 2021/05/11 18:33:48 by asebrech         ###   ########.fr       */
+/*   Updated: 2021/05/12 10:01:32 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
+static void	s_flag(char *str, t_struct *data, char *dst, int len_str)
+{
+	int	len_dst;
+
+	len_dst = ft_strlen(dst);
+	ft_memset(&dst[0], ' ', data->width);
+	len_dst = ft_strlen(dst);
+	if (data->flag == '-')
+		ft_memcpy(&dst[0], str, len_str);
+	else
+		ft_memcpy(&dst[len_dst - len_str], str, len_str);
+}
+
 int	ft_convert_str(char *str, t_struct *data)
 {
-	int	ret;
+	char	*dst;
+	int		ret;
+	int		len_str;
 
-	data = NULL;
-	ret = ft_printf(str);
+	ret = 0;
+	if (data->precision == -1 && data->point)
+		data->precision = 0;
+	if (data->point)
+		str = ft_substr(str, 0, data->precision);
+	len_str = ft_strlen(str);
+	if (data->width > len_str)
+	{
+		dst = ft_calloc(data->width + 1, sizeof(char));
+		if (!dst)
+			return (0);
+		s_flag(str, data, dst, len_str);
+		ret = ft_putstr_fd(dst, 1);
+		free(dst);
+	}
+	else
+		ret = ft_putstr_fd(str, 1);
 	return (ret);
 }
